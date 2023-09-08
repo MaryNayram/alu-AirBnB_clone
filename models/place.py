63dabc31cce754ee510b2b4315dbd9cdefc4ba1f
Mary <122350784+MaryNayram@users.noreply.github.com>
2023-09-08 15:05:04 +0000

    Update place.py

--- models/place.py
+++ models/place.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-        """Represent a place.
+    """Represent a place.
     Attributes:
         city_id (str): The City id.
         user_id (str): The User id.
@@ -19,15 +19,15 @@ class Place(BaseModel):
         amenity_ids (list): A list of Amenity ids.
     """
 
-            city_id = ""
-                user_id = ""
-                    name = ""
-                        description = ""
-                            number_rooms = 0
-                                number_bathrooms = 0
-                                    max_guest = 0
-                                        price_by_night = 0
-                                            latitude = 0.0
-                                                longitude = 0.0
-                                                    amenity_ids = []
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
                                                     
