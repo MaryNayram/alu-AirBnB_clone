f8873d6c3d18949cfbdb492ba234d5fbbd373e4b
KurMalual <k.malual@alustudent.com>
2023-09-10 03:32:12 -0700

    state.py

--- models/state.py
+++ models/state.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """Class State that inherits from the BaseModel class"""
 
 from models import BaseModel
@@ -12,17 +11,3 @@ class State(BaseModel):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-=======
-"""Defines the State class."""
-from models.base_model import BaseModel
-
-
-class State(BaseModel):
-        """Represent a state.
-    Attributes:
-        name (str): The name of the state.
-    """
-
-            name = ""
-            
->>>>>>> 40e07bb0bc8d2b22c2ecf1f76fc9790eae1e9dbc
