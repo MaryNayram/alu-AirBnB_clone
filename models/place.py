5552482a7f7e92443f817e3afa28718a914ac742
KurMalual <k.malual@alustudent.com>
2023-09-10 03:27:11 -0700

    place.py

--- models/place.py
+++ models/place.py
@@ -5,7 +5,6 @@ from models import BaseModel
 
 
 class Place(BaseModel):
-<<<<<<< HEAD
     """Place class"""
 
     city_id = ""
@@ -21,33 +20,4 @@ class Place(BaseModel):
     amenity_ids = []
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)                                                    
-=======
-    """Represent a place.
-    Attributes:
-        city_id (str): The City id.
-        user_id (str): The User id.
-        name (str): The name of the place.
-        description (str): The description of the place.
-        number_rooms (int): The number of rooms of the place.
-        number_bathrooms (int): The number of bathrooms of the place.
-        max_guest (int): The maximum number of guests of the place.
-        price_by_night (int): The price by night of the place.
-        latitude (float): The latitude of the place.
-        longitude (float): The longitude of the place.
-        amenity_ids (list): A list of Amenity ids.
-    """
-
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
-                                                    
->>>>>>> 40e07bb0bc8d2b22c2ecf1f76fc9790eae1e9dbc
+        super().__init__(*args, **kwargs)
